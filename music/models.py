efdc369f803fed3320d9d1e38c62ffc181612303
subkhanov <sbkhnv@gmail.com>
2024-05-12 10:29:56 +0300

    updated

--- music/models.py
+++ music/models.py
@@ -11,12 +11,26 @@ class Albom(models.Model):
     title = models.CharField(max_length=10)
     artist = models.ForeignKey(Artist,on_delete=models.CASCADE,null=True)
     cover = models.URLField(null=True)
+    popular = models.PositiveIntegerField(default=0)
     create_date = models.DateField(auto_now_add=True,null=True)
     Last_update = models.DateField(auto_now=True)
+    class Meta:
+        ordering = ['id']
+        indexes = [
+            models.Index(fields=['id'])
+        ]
 
 class Songs(models.Model):
     title = models.CharField(max_length=10)
     cover = models.URLField(null=True)
     albom = models.ForeignKey(Albom,on_delete=models.CASCADE,null=True)
+    listened = models.PositiveIntegerField(default=0)
     create_date = models.DateField(auto_now_add=True,null=True)
     Last_update = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['id']
+        indexes = [
+            models.Index(fields=['id'])
+        ]
+
--- music/views.py
+++ music/views.py
@@ -1,4 +1,6 @@
+from django.db.transaction import atomic
 from django.shortcuts import render
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Artist,Albom,Songs
@@ -7,7 +9,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.filters import SearchFilter
-from rest_framework import filters
+from rest_framework import filters, status
 from rest_framework.pagination import LimitOffsetPagination
 class LandingPageAPIView(APIView):
     def get(self,request):
@@ -35,7 +37,29 @@ class AlbomDetailApiView(ModelViewSet):
     queryset = Albom.objects.all()
     serializer_class = AlbomSerializer
     authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+    # permission_classes = (IsAuthenticated, )
+
+    @action(detail=False,methods=['POST'])
+    def artist(self,request,*args,**kwargs):
+        albom = self.get_object()
+        albom = albom.albom.artist
+        serializer = ArtistSerializer(albom)
+        return Response(data=serializer.data)
+
+    @action(detail=True, methods=["GET"])
+    def popular(self, request, *args, **kwargs):
+        albom = self.get_object()
+        with atomic():
+            albom.popular += 1
+            albom.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=False, methods=['GET'])
+    def top_albom(self, request, *args, **kwargs):
+        albom = self.get_queryset()
+        albom = albom.order_by('-popular')[:1]
+        serializer = SongsSerializer(albom, many=True)
+        return Response(data=serializer.data)
 
 
 # class SongsAPIView(APIView):
@@ -49,10 +73,37 @@ class SongSetApiView(ModelViewSet):
     queryset = Songs.objects.all()
     serializer_class = SongsSerializer
     # authentication_classes = (TokenAuthentication, )
-    permission_classes = (IsAuthenticated, )
+    # permission_classes = (IsAuthenticated, )
     filter_backends = (filters.SearchFilter, )
     search_fields = ['title', ]  # ['^title', ] ['@title', ] ['=title', ]
     pagination_class = LimitOffsetPagination
+
+    @action(detail=True, methods=["GET"])
+    def listen(self,request,*args,**kwargs):
+        song = self.get_object()
+        with atomic():
+            song.listened += 1
+            song.save()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+    @action(detail=False,methods=['GET'])
+    def top(self,request,*args,**kwargs):
+        songs = self.get_queryset()
+        songs = songs.order_by('-listened')[:3]
+        serializer = SongsSerializer(songs,many=True)
+        return Response(data=serializer.data)
+
+
+    @action(detail=False,methods=['POST'])
+    def albom(self,request,*args,**kwargs):
+        songs = self.get_object()
+        albom = songs.albom
+        serializer = AlbomSerializer(albom)
+        return Response(data=serializer.data)
+
+
+
     # permission_classes = (AllowAny, )
     # def get(self,request,id):
     #     songs = Songs.objects.get(id=id)
